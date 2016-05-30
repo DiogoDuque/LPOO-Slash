1761a976c33a8369d242c094492179958ef62306
Whattz <diogoduque13@hotmail.com>
2016-05-30 01:18:02 +0100

    Unit tests are fully functional!

--- core/src/test/java/BallTest.java
+++ core/src/test/java/BallTest.java
@@ -3,22 +3,34 @@ import com.badlogic.gdx.physics.box2d.World;
 import com.lpoo.gameobjects.Ball;
 
 import org.junit.Test;
-import static org.junit.Assert.assertEquals;
+
+import static org.junit.Assert.*;
 
 /**
  * Created by Diogo on 29-05-2016.
  */
 public class BallTest {
 
+    private World world;
+    private int x,y;
+    private Ball ball;
 
-    @Test
-    public void test1()
+    public BallTest()
     {
-        World world=new World(new Vector2(0, 0), false);
-        int x=50,y=50;
-        Ball ball = new Ball(x,y,world);
-        assertEquals(x,ball.getX());
-        world=null;
+        world=new World(new Vector2(0, 0), false);
+        x=60;
+        y=20;
     }
 
+    @Test
+    public void CreateBall()
+    {
+        ball = new Ball(x,y,world);
+        assertSame(x,(int)ball.getX()); //xPosition
+        assertSame(y,(int)ball.getY()); //yPosition
+        assertSame(x,(int)ball.getBody().getPosition().x); //xPosition
+        assertSame(y,(int)ball.getBody().getPosition().y); //yPosition
+        //for full coverage
+        assertTrue((Ball.getRadius()-ball.getRadius())==0);
+    }
 }
