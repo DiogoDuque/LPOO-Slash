89bd73b3a5fbaf9d72e1d84d13702ec81e7d2680
joaogomes04 <up201403275@fe.up.pt>
2016-06-01 04:28:16 +0100

    BUG FIX :: CHECKBALLS

--- core/src/com/lpoo/gameworld/GameWorld.java
+++ core/src/com/lpoo/gameworld/GameWorld.java
@@ -6,6 +6,7 @@ import com.badlogic.gdx.physics.box2d.World;
 import com.lpoo.gameobjects.Ball;
 import com.lpoo.gameobjects.GameArea;
 import com.lpoo.gameobjects.Slasher;
+import com.lpoo.slashhelpers.Function;
 
 import java.util.ArrayList;
 import java.util.Random;
@@ -26,7 +27,7 @@ public class GameWorld {
         gameArea = new GameArea(pt1,pt2,pt3,pt4,world);
         slasher = new Slasher(pt1,this);
         balls = new ArrayList<Ball>();
-        createBalls(1);
+       createBalls(1);
     }
 
     /**
@@ -110,17 +111,27 @@ public class GameWorld {
         Vector2[] points = new Vector2[4];
         Vector2 newPoint = new Vector2(slasher.getFinger());
         Vector2[] oldPoints = gameArea.getPoints();
+        Vector2[] pointsTriangle = new Vector2[3];
+        pointsTriangle[0] = slasher.getPosition();
+      //  pointsTriangle[1] = toDelete;
+
         for(int i=0; i<4; i++)
         {
-            if(gameArea.getToDelete()==oldPoints[i])
+            if(gameArea.getToDelete()==oldPoints[i]){
                 points[i]=newPoint;
+                pointsTriangle[2] = newPoint;
+                pointsTriangle[1]=gameArea.getToDelete();}
             else points[i]=oldPoints[i];
         }
         Vector2 toDelete = gameArea.getToDelete();
         gameArea.dispose();
         gameArea=new GameArea(points[0],points[1],points[2],points[3],world);
+      //  pointsTriangle[0] = slasher.getPosition();
+        //pointsTriangle[1] = toDelete;
+       // pointsTriangle[2] = newPoint;
+        System.out.println("GameWorld::PointsTriangle() = p1"+pointsTriangle[0]+"     p2 "+pointsTriangle[1]+" p3"+pointsTriangle[2]);
         slasher=new Slasher(newPoint,this);
-        int counter = checkBalls();
+        int counter = checkBalls(pointsTriangle);
         createBalls(counter+1);
     }
 
@@ -130,15 +141,16 @@ public class GameWorld {
      *
      * @return number of balls that are out of the gameArea.
      */
-    public int checkBalls(){
-        int counter=0;
+    public int checkBalls(Vector2[] pointsTriangle){
+       int counter=0;
         for (int i = 0; i < balls.size();i++) {
-            if (!pointInPolygon(4, gameArea.getPoints(), balls.get(i))){
+            if (pointInPolygon( pointsTriangle, balls.get(i))){
                 Gdx.app.log("bola esta fora", "bola esta fora");
                 Ball ball = balls.get(i);
                 balls.remove(i);
                 ball.dispose();
-                counter++;
+               counter++;
+                //createBalls(1);
                 System.out.println("GameWorld::createBalls() ");
             }
 
@@ -147,20 +159,48 @@ public class GameWorld {
 
     }
 
-    public boolean pointInPolygon(int polyCorners, Vector2[] points, Ball ball) {
-        int i, j = polyCorners - 1;
-        boolean oddNodes = false;
+    public boolean pointInPolygon( Vector2[] pointsTriangle, Ball ball) {
 
-        for (i = 0; i < polyCorners; i++) {
-            if ((points[i].y < ball.getY() && points[j].y >= ball.getY()
-                    || points[j].y < ball.getY() && points[i].y >= ball.getY())
-                    && (points[i].x <= ball.getX() || points[j].x <= ball.getX())) {
-                if (points[i].x + (ball.getY() - points[i].y) / (points[j].y - points[i].y) * (points[j].x - points[i].x) < ball.getX()) {
-                    oddNodes = !oddNodes;
-                }
-            }
-            j = i;
+        Vector2 center = new Vector2();
+        boolean b = true;
+        center.x = (pointsTriangle[0].x + pointsTriangle[1].x + pointsTriangle[2].x)/3;
+        center.y = (pointsTriangle[0].y + pointsTriangle[1].y + pointsTriangle[2].y)/3;
+        Vector2 ballPoint = new Vector2(ball.getBody().getPosition().x,ball.getBody().getPosition().y);
+        Function f = new Function(center, ballPoint);
+        for(int i = 0;i<pointsTriangle.length;i++){
+            Function edge = new Function(pointsTriangle[i], pointsTriangle[((i+1)%3)]);
+            if(isBetween(f,edge,center,ballPoint))
+                b = false;
         }
-        return oddNodes;
-    }
+        return b;
 }
+    public boolean isBetween (Function f, Function edge, Vector2 center, Vector2 ballPoint){
+
+        Vector2 p = f.intersect(edge);
+
+        System.out.println("GameWorld::isBetween() center:"+ center + " p" + p + "ball" + ballPoint);
+        if(p == null)
+            return false;
+      //  if ((p.x >= center.x && p.x <= ballPoint.x) || (p.x <= center.x && p.x >= ballPoint.x))
+        //    if ((p.y >= center.y && p.y <= ballPoint.y) ||(p.y <= center.y && p.y >= ballPoint.y))
+          //      return true;
+        if ((p.x > center.x )&& (p.x < ballPoint.x)){
+            if ((p.y > center.y) && (p.y < ballPoint.y))
+                return true;
+            else if ((p.y < center.y) &&( p.y > ballPoint.y))
+                return true;
+            else return false;
+        }
+        if ((p.x < center.x) && (p.x > ballPoint.x)){
+            if ((p.y > center.y) && (p.y < ballPoint.y))
+                return true;
+            else if ((p.y < center.y) &&( p.y > ballPoint.y))
+                return true;
+            else return false;
+        }
+        return false;
+    }
+
+
+}
+
