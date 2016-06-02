8d9c408eb7c6893c871de424f2d6a0add5adbe59
joaogomes04 <up201403275@fe.up.pt>
2016-06-02 04:02:03 +0100

    resize almost done - bug no slasher que nao atualiza

--- core/src/com/lpoo/slashhelpers/Utilities.java
+++ core/src/com/lpoo/slashhelpers/Utilities.java
@@ -2,6 +2,10 @@ package com.lpoo.slashhelpers;
 
 import com.badlogic.gdx.math.Vector2;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 /**
  * Created by Diogo on 01-06-2016.
  */
@@ -16,5 +20,36 @@ public final class Utilities {
     public static double distance(Vector2 pt1, Vector2 pt2)
     {
         return Math.sqrt(Math.pow(pt1.x-pt2.x,2)+Math.pow(pt1.y-pt2.y,2));
+    }
+    public static List<Vector2> getCircleLineIntersectionPoint(Vector2 pointA, Vector2 pointB, Vector2 center, float radius) {
+        float baX = pointB.x - pointA.x;
+        float baY = pointB.y - pointA.y;
+        float caX = center.x - pointA.x;
+        float caY = center.y - pointA.y;
+
+        float a = baX * baX + baY * baY;
+        float bBy2 = baX * caX + baY * caY;
+        float c = caX * caX + caY * caY - radius * radius;
+
+        float pBy2 = bBy2 / a;
+        float q = c / a;
+
+        float disc = pBy2 * pBy2 - q;
+        if (disc < 0) {
+            return Collections.emptyList();
+        }
+        // if disc == 0 ... dealt with later
+        float tmpSqrt = (float) Math.sqrt(disc);
+        float abScalingFactor1 = -pBy2 + tmpSqrt;
+        float abScalingFactor2 = -pBy2 - tmpSqrt;
+
+        Vector2 p1 = new Vector2(pointA.x - baX * abScalingFactor1, pointA.y
+                - baY * abScalingFactor1);
+        if (disc == 0) { // abScalingFactor1 == abScalingFactor2
+            return Collections.singletonList(p1);
+        }
+        Vector2 p2 = new Vector2(pointA.x - baX * abScalingFactor2, pointA.y
+                - baY * abScalingFactor2);
+        return Arrays.asList(p1, p2);
     }
 }
