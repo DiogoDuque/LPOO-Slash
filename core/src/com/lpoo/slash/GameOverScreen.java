cd06464b81158d1cf564a35cd7f89dc87aac4ff7
DiogoDuque <diogoduque13@hotmail.com>
2016-06-03 14:49:25 +0100

    GameOverScreen almost done
    TODO:
    - screen transition should be to main manu, or there should be a choice
    between menu and game
    - insert real score values

--- core/src/com/lpoo/slash/GameOverScreen.java
+++ core/src/com/lpoo/slash/GameOverScreen.java
@@ -2,10 +2,17 @@ package com.lpoo.slash;
 
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.InputProcessor;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.lpoo.slashhelpers.InputHandler;
+import com.lpoo.slashhelpers.Utilities;
 
 /**
  * Created by Diogo Duque on 03/06/2016.
@@ -13,20 +20,74 @@ import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 public class GameOverScreen implements Screen {
 
     private Game game;
+    private float width, height;
     private OrthographicCamera cam;
-    private ShapeRenderer shapeRenderer;
+    private BitmapFont font;
+    private SpriteBatch batch;
+    Sprite background;
 
     public GameOverScreen(Game game)
     {
         this.game=game;
         cam = new OrthographicCamera();
-        cam.setToOrtho(true, Slash.screenDimensions.x, Slash.screenDimensions.y);
-        shapeRenderer=new ShapeRenderer();
+        width=Slash.screenDimensions.x*1.2f;
+        height=Slash.screenDimensions.y*1.2f;
+        cam.setToOrtho(false,width,height);
+        font = new BitmapFont();
+        font.getData().setScale(0.7f,0.7f);
+        batch=new SpriteBatch();
+        background=new Sprite(new Texture("gameover/background.png"));
+        implementTouchDetector();
     }
 
-    @Override
-    public void show() {
+    /**
+     * used to detect input 'TouchUp', and when detected it will change screen.
+     */
+    private void implementTouchDetector()
+    {
+        Gdx.input.setInputProcessor(new InputProcessor() {
 
+            @Override
+            public boolean touchUp(int screenX, int screenY, int pointer, int button) {
+                Utilities.changeScreen(game, new GameScreen(game));
+                return true;
+            }
+
+            @Override
+            public boolean keyDown(int keycode) {
+                return false;
+            }
+
+            @Override
+            public boolean keyUp(int keycode) {
+                return false;
+            }
+
+            @Override
+            public boolean keyTyped(char character) {
+                return false;
+            }
+
+            @Override
+            public boolean touchDown(int screenX, int screenY, int pointer, int button) {
+                return false;
+            }
+
+            @Override
+            public boolean touchDragged(int screenX, int screenY, int pointer) {
+                return false;
+            }
+
+            @Override
+            public boolean mouseMoved(int screenX, int screenY) {
+                return false;
+            }
+
+            @Override
+            public boolean scrolled(int amount) {
+                return false;
+            }
+        });
     }
 
     @Override
@@ -37,17 +98,19 @@ public class GameOverScreen implements Screen {
         Gdx.gl.glClearColor(0, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
-        // Begin ShapeRenderer
-        shapeRenderer.begin(ShapeRenderer.ShapeType.Filled);
+        batch.setProjectionMatrix(cam.combined); //or your matrix to draw GAME WORLD, not UI
+        batch.begin();
 
-        // Draw Background color
-        shapeRenderer.setColor(55 / 255.0f, 80 / 255.0f, 100 / 255.0f, 1);
-        shapeRenderer.rect(0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+        batch.draw(background, 0, 0, width, height);
+        font.draw(batch, "Score: 35 <- temporary and static value", 40, 125);
+        font.draw(batch,"Touch screen to play again", 150,50);
 
-        shapeRenderer.setColor(0, 0, 0, 1);
-        shapeRenderer.circle(100,100,50);
+        batch.end();
 
-        shapeRenderer.end();
+    }
+
+    @Override
+    public void show() {
 
     }
 
