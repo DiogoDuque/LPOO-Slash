a6b8c2b4b5d6909665369f97ed50287d35bd381e
DiogoDuque <diogoduque13@hotmail.com>
2016-06-07 00:45:35 +0100

    Redirecter test done! (only missing setRedirecter in gameWorld)

--- core/src/test/java/com/lpoo/gameobjects/RedirecterTest.java
+++ core/src/test/java/com/lpoo/gameobjects/RedirecterTest.java
@@ -1,5 +1,9 @@
 package com.lpoo.gameobjects;
 
+import com.badlogic.gdx.math.Vector2;
+import com.lpoo.gameworld.GameWorld;
+
+import static org.junit.Assert.*;
 import org.junit.Test;
 
 /**
@@ -10,6 +14,19 @@ public class RedirecterTest {
     @Test
     public void testChangeBallDirection()
     {
-        //for(int i=10; i<10)
+        Vector2 pt1=new Vector2(20,20),pt2=new Vector2(30,175),pt3=new Vector2(235,175),pt4=new Vector2(225,20);
+        GameWorld gameWorld=new GameWorld(pt1,pt2,pt3,pt4,pt2);
+        gameWorld.setRedirecter(new Redirecter(gameWorld.getGameArea().getPoints()));
+
+        Slasher slasher = gameWorld.getSlasher();
+
+        //ball nearby Redirecter
+        Ball ball = gameWorld.getBalls().get(0);
+        Vector2 oldDirection = ball.getBody().getLinearVelocity().cpy();
+        ball.getBody().setTransform(100, 100, 0);
+
+        gameWorld.update(0);
+
+        assertNotEquals(oldDirection,ball.getBody().getLinearVelocity());
     }
 }
